6a5024e23730de615e4e1cee1e897ac9f8515b61
Ralph Meijer <ralphm@ik.nu>
2009-08-19 14:47:26 +0000

    Add groupchat and anyMeta status support.
    --HG--
    rename : ikdisplay/vote.py => ikdisplay/xmpp.py
    extra : convert_revision : %3A98

---
+++ ikdisplay/xmpp.py
@@ -0,0 +1,111 @@
+from twisted.internet import reactor, task
+from twisted.python import log
+from twisted.words.protocols.jabber import error
+from twisted.words.protocols.jabber.jid import internJID as JID
+from twisted.words.protocols.jabber.xmlstream import IQ
+
+from wokkel.client import XMPPClient
+from wokkel.ping import PingClientProtocol
+from wokkel.pubsub import PubSubClient
+from wokkel.xmppim import AvailabilityPresence, MessageProtocol
+
+class PubSubClientFromController(PubSubClient):
+
+    def __init__(self, controller, service, nodeIdentifiers):
+        self.controller = controller
+        self.service = service
+        self.nodeIdentifiers = nodeIdentifiers
+
+    def connectionInitialized(self):
+        PubSubClient.connectionInitialized(self)
+
+        clientJID = self.parent.factory.authenticator.jid
+        for nodeIdentifier in self.nodeIdentifiers:
+            self.subscribe(self.service, nodeIdentifier, clientJID)
+
+
+    def itemsReceived(self, event):
+        if event.nodeIdentifier in self.nodeIdentifiers:
+            self.controller.gotEvent(event)
+
+
+class GroupChatHandler(MessageProtocol):
+
+    def __init__(self, controller, occupantJID):
+        self.controller = controller
+        self.occupantJID = occupantJID
+
+
+    def connectionInitialized(self):
+        MessageProtocol.connectionInitialized(self)
+        presence = AvailabilityPresence(self.occupantJID)
+        self.send(presence.toElement())
+
+
+    def onMessage(self, message):
+        sender = JID(message['from'])
+
+        if (sender.userhost() == self.occupantJID.userhost() and
+            message['type'] == 'groupchat' and
+            message.body):
+            body = unicode(message.body)
+            self.controller.gotGroupChatMessage(sender.resource, body)
+
+
+
+class Pinger(PingClientProtocol):
+
+    def __init__(self, entity):
+        self.entity = entity
+        self.lc = task.LoopingCall(self.doPing)
+
+
+    def connectionInitialized(self):
+        self.lc.start(60)
+
+
+    def connectionLost(self, reason):
+        self.lc.stop()
+
+
+    def doPing(self):
+        log.msg("*** PING ***")
+
+        def cb(result):
+            log.msg("*** PONG ***")
+
+        def eb(failure):
+            failure.trap(error.StanzaError)
+            exc = failure.value
+
+            if exc.condition != 'remote-server-not-found':
+                return failure
+
+            log.msg("Remote server not found, restarting stream.")
+            reactor.callLater(5, self.send, '</stream:stream>')
+
+        d = self.ping(self.entity)
+        d.addCallbacks(cb, eb)
+        d.addErrback(log.err)
+        return d
+
+
+
+def makeService(config, controller):
+    if IQ.timeout is None:
+        IQ.timeout = 30
+
+    xmppService = XMPPClient(config['jid'], config['secret'])
+    if config['verbose']:
+        xmppService.logTraffic = True
+    xmppService.send('<presence><priority>-1</priority></presence>')
+
+    pc = PubSubClientFromController(controller,
+                                    config['service'],
+                                    config['nodeIdentifiers'])
+    pc.setHandlerParent(xmppService)
+
+    pinger = Pinger(config['service'])
+    pinger.setHandlerParent(xmppService)
+
+    return xmppService
